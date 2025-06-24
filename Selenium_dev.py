a32210129be3128fb47ba435c0963c8e06671168
Alexandros Papanikos <92908848+Ale-py@users.noreply.github.com>
2025-06-24 10:48:27 +0300

    Update Selenium_dev.py

--- Selenium_dev.py
+++ Selenium_dev.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# Launch the browser
 driver = webdriver.Chrome()
-
+# Navigate to login page
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-
+# Get title
 title = driver.title
 
 driver.implicitly_wait(0.5)
-
+# Find elements
 text_box = driver.find_element(by=By.NAME, value="my-text")
 submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
+# Enter keys
 text_box.send_keys("Selenium")
 submit_button.click()
 
